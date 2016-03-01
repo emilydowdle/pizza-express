c985a20398d4cd09991168aaf3ed43a45a7bf94c
Emily Dowdle <emily@editingemily.com>
2016-03-01 08:53:46 -0700

    Add first three unit tests

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('static'));
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'jade');
+
+app.locals.title = 'Pizza Express';
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
